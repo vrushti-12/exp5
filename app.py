70e17cd31f3b474a68b6d251c05d5f69d2cb80ee
vrushti-12 <vrushtipawar12@gmail.com>
2026-08-20 17:12:15 +0530

    Add print statements to app.py
    Added print statements for tutorial and experiment messages.

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
  return 'Hello, World!'
+
+print("Hello!! this is app.py file!")
+print("Welcome to this level")
+print("this is experiment 3 of tutorial on jenkins!")
+print("learn this tutorial carefully")
